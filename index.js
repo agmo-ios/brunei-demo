d0e69347cb07ffdd87096a606cbb2840abeb2028
JiaHui <jiahui@agmostudio.com>
2019-06-26 18:06:40 +0800

    update port

--- index.js
+++ index.js
@@ -33,6 +33,6 @@ app.post('/users/:name/messages', function(req, res) { // (5)
 	res.sendStatus(204);
 });
 
-app.listen(4000, function() { // (6)
-	console.log('App listening on port 4000');
+app.listen(80, function() { // (6)
+	console.log('App listening on port 80');
 });
